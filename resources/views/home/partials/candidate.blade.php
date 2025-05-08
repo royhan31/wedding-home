78c048d3b115c68facf9988988e52896c275d6ce
Royhan <izzatur.royhan@gmail.com>
2025-05-08 23:45:37 +0700

    fixing image candidate

--- resources/views/home/partials/candidate.blade.php
+++ resources/views/home/partials/candidate.blade.php
@@ -1,4 +1,4 @@
-<section class="wpo-couple-section-s2 section-padding pt-0" id="couple">
+{{-- <section class="wpo-couple-section-s2 section-padding pt-0" id="couple">
     <div class="container-fluid">
         <div class="couple-area clearfix">
             <div class="couple-wrap">
@@ -47,6 +47,66 @@
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
+    </div> <!-- end container -->
+</section> --}}
+
+<section class="wpo-couple-section section-padding pt-2" id="couple">
+    <div class="container">
+        <div class="couple-area clearfix">
+            <div class="couple-wrap">
+                <div class="row align-items-center gx-5">
+                    <div class="col col-md-6 col-12">
+                        <div class="couple-item">
+                            <div class="couple-img">
+                                <img src="{{$male ? $male->image : ""}}" alt="">
+                            </div>
+                            <div class="couple-text">
+                                <i><img src="assets/images/couple/groom.svg" alt=""></i>
+                                <h3>{{$male ? $male->full_name : ""}}</h3>
+                                <p>{{$male ? $male->description : ""}}</p>
+                                <div class="social">
+                                    <ul>
+                                        <li><a href="#"><i class="{{$male ? $male->facebook_url : ""}}"></i></a></li>
+                                        <li><a href="#"><i class="{{$male ? $male->instagram_url : ""}}"></i></a></li>
+                                    </ul>
+                                </div>
+                                <div class="couple-bg">
+                                    <svg viewBox="0 0 500 433" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M500 28.0003C500 28.0003 460 -20.2942 422 11.0001C409.925 20.9443 405.623 37.833 404.858 54.6979C404.084 71.7365 382.445 85.4249 368.836 75.1428C366.964 73.7282 364.83 72.6975 362.559 72.11L357.558 70.8167C331.547 64.0898 305.684 82.2112 303.126 108.956L302.607 114.379C299.705 144.721 269.789 164.837 240.595 156.079L237 155C209.695 139.265 175.332 157.485 173.034 188.914L171.265 213.107C170.157 228.267 157.534 240 142.333 240C140.118 240 137.911 239.747 135.754 239.244L123.945 236.494C95.3837 229.843 68.6684 253.052 71.2621 282.263L72.436 295.484C74.2567 315.988 56.7136 332.954 36.2814 330.449C17.0009 328.085 0 343.128 0 362.553V433H500V28.0003Z" fill="white"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col col-md-6 col-12">
+                        <div class="couple-item">
+                            <div class="couple-img">
+                                <img src="{{$female ? $female->image : ""}}" alt="">
+                            </div>
+                            <div class="couple-text">
+                                <i><img src="assets/images/couple/bride.svg" alt=""></i>
+                                <h3>{{$female ? $female->full_name : ""}}</h3>
+                                <p>{{$female ? $female->description : ""}}</p>
+                                <div class="social">
+                                    <ul>
+                                        <li><a href="{{$female ? $female->facebook_url : ""}}"><i class="ti-facebook"></i></a></li>
+                                        <li><a href="{{$female ? $female->instagram_url : ""}}"><i class="ti-instagram"></i></a></li>
+                                    </ul>
+                                </div>
+                                <div class="couple-bg">
+                                    <svg viewBox="0 0 500 433" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M0 28.0003C0 28.0003 40 -20.2942 78 11.0001C90.075 20.9443 94.3766 37.833 95.1425 54.6979C95.9162 71.7365 117.555 85.4249 131.164 75.1428C133.036 73.7282 135.17 72.6975 137.441 72.11L142.442 70.8167C168.453 64.0898 194.316 82.2112 196.874 108.956L197.393 114.379C200.295 144.721 230.211 164.837 259.405 156.079L263 155C290.305 139.265 324.668 157.485 326.966 188.914L328.735 213.107C329.843 228.267 342.466 240 357.667 240C359.882 240 362.089 239.747 364.246 239.244L376.055 236.494C404.616 229.843 431.332 253.052 428.738 282.263L427.564 295.484C425.743 315.988 443.286 332.954 463.719 330.449C482.999 328.085 500 343.128 500 362.553V433H0V28.0003Z" fill="white"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                  
+                </div>
+                <div class="shape-1"><img src="assets/images/couple/flower1.png" alt=""></div>
+                <div class="shape-2"><img src="assets/images/couple/flower2.png" alt=""></div>
             </div>
         </div>
     </div> <!-- end container -->
